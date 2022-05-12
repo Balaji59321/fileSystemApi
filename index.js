ed0a90847264054b96094e980c218d8995793201
Balaji59321 <balaji59321@gmail.com>
2022-05-12 18:00:02 +0530

    added cors and env port details

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const route = require("./file");
 const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const route = require("./file");
 
+dotenv.config();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/", (req, res, next) => {
@@ -11,6 +16,6 @@ app.use("/", (req, res, next) => {
 
 app.use("/get", route);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server Started Successfully");
 });
