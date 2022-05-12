f0a8306093630c1d50187ce2d304de8f5a0a7752
Balaji59321 <balaji59321@gmail.com>
2022-05-12 18:01:52 +0530

    added cors and env port details

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 
 //joining path of directory
-const directoryPath = path.join(__dirname, "/../../../Downloads");
+const directoryPath = path.join(__dirname, "/");
 console.log(__dirname);
 //passsing directoryPath and callback function
 
